87a13a1c4555816bc26a922c9392228966df50ae
Gandotra <agandot1@ford.com>
2023-03-20 00:34:50 +0530

    final commit

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -13,9 +13,8 @@ export class RegistrationComponent {
 
   wallet:Wallet = new Wallet();
   datePipe: any;
-
-  // wallet.doc = this.datePipe.transform(this.wallet, 'yyyy-MM-dd');
-
+  msg: any;
+  errormsg:any;
   constructor(private walletservice:WalletService, private walletBackendService:WalletBackendService) {}
 
   submitForm(){
@@ -26,17 +25,25 @@ export class RegistrationComponent {
     let wltPost: Observable<any> = this.walletBackendService.addWallet(this.wallet);
     wltPost.subscribe(
       {
-        next:(data) => {
+        next:(data) =>{
+          
+          this.wallet = data;
           console.log(data);
         },
+
         error:(error) =>{
-          console.log(error);
+          console.log(error.error);
+          this.msg= "";
+          this.errormsg= JSON.stringify(error.error);
         },
         complete:() =>{
-          console.log("Post Request Completed...");
+          this.msg = "Wallet Created Successfully";
+          this.errormsg = "";
+          console.log("Request Completed...");
         }
       }
     )
+    
 
   }
 
